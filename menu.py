92915671e12390117342ac69e5ed2e57496d4f15
LSanten <leon.santen@icloud.com>
2018-10-18 12:24:24 -0400

    create visual representation of the mennu with controller

---
+++ menu.py
@@ -0,0 +1,14 @@
+"""
+Menu structure that changes the settings for the game with a controller
+
+@authors: Richard Ballaux, Viktor Deturck, Leon Santen
+"""
+
+import pygame
+from pygame.locals import *
+import time
+
+
+
+if __name__ == '__main__':
+    pygame.init()
