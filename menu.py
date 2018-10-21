4a5f830fe82ef002ea8a3029910f307d708ff292
LSanten <leon.santen@icloud.com>
2018-10-20 20:11:54 -0400

    second git push for safety

--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ Menu structure that changes the settings for the game with a controller
 @authors: Richard Ballaux, Viktor Deturck, Leon Santen
 """
 
+
 import pygame
 from pygame.locals import *
 import time
