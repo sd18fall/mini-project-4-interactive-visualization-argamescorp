28b78b764b47b4518bb2e808f6eb5699a01dedeb
Viktor Deturck <vdeturck@olin.edu>
2018-10-22 12:41:33 -0400

    original code + first draft of implementation code for the obj recog

--- ObjectRecogImplementation.py
+++ ObjectRecogImplementation.py
@@ -28,10 +28,12 @@ def getCoords():
 
     # Get the video data
     ret, orImg=cam.read()
-    # Resize the frame, to have not too many pixels
+
+    # Resize the frame, to have not too many pixels and flip the image.
     orImg=cv2.resize(orImg,(horRes,vertRes))
     global img
     img = cv2.flip(orImg, 1)
+
     # convert BGR to HSV
     imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     # create the Mask, look for the object in this color range
@@ -43,29 +45,30 @@ def getCoords():
     maskFinal=maskClose # This is our final image with object in black-white (object is white)
     im2, conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     coords = []
-    radius = []
+    widthList = []
     centerCoords = [(-1,-1),(-1,-1)]
     #cv2.drawContours(img,conts,-1,(255,0,0),3)
     for i in range(len(conts)):
-        #x,y,w,h=cv2.boundingRect(conts[i])
+        x,y,w,h=cv2.boundingRect(conts[i])
         #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
-        (x,y),rad = cv2.minEnclosingCircle(conts[i])
+        #(x,y),rad = cv2.minEnclosingCircle(conts[i])
         center = (int(x),int(y))
         coords.append(center)
-        radius.append(int(rad))
+        #radius.append(int(rad))
+        widthList.append(w)
         i = 0
-    if len(radius) > 0:
+    if len(widthList) > 0:
         while i < 2:
-            center = coords[radius.index(max(radius))]
+            center = coords[widthList.index(max(widthList))]
             if center[0] < horRes/2:
                 centerCoords[0] = center
             else:
                 centerCoords[1] = center
             coords.remove(center)
-            rad = max(radius)
-            radius.remove(max(radius))
+            width = max(widthList)
+            widthList.remove(max(widthList))
             #cv2.circle(img,center,rad,(0,22,0),2)
-            if i == len(radius):
+            if i == len(widthList):
                 break
             i +=1
     print (centerCoords)
